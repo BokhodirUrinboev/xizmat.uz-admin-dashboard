d84e755dc1aa9f121a5f32f0abec4a56483b8e4c
Ndn1618 <ndilbar1618@gmail.com>
2021-12-04 12:18:52 +0500

    Add apollo client

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import useToken from './hooks/useToken';
 import HomePage from "./pages/HomePage";
 import Signin from "./pages/Signin";
+import {Route, Switch, Redirect} from "react-router-dom"
 
 function App() {
 
@@ -9,9 +10,14 @@ function App() {
 
   return (
     <>
-      {token ? (
-        <HomePage />
-      ) : <Signin />}
+      <Switch>
+        <Route exact path="/signin">
+          {token ? <Redirect to="/" /> : <Signin />}
+        </Route>
+        <Route path="/">
+          {token ? <HomePage /> : <Redirect to="/signin" />}
+        </Route>
+      </Switch>
     </>
   )
 }
--- src/context/Authentication.js
+++ src/context/Authentication.js
@@ -1,22 +1,69 @@
 import React from "react";
 import { createContext, useEffect, useState } from 'react';
 
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+  split
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { getMainDefinition } from '@apollo/client/utilities';
+
 const Context = createContext(null);
 
-function Provider ({children}) {
+function Provider({ children }) {
 
   const [state, setState] = useState(JSON.parse(window.localStorage.getItem('token')))
   useEffect(() => {
     if (state) {
-        window.localStorage.setItem('token', JSON.stringify(state))
+      window.localStorage.setItem('token', JSON.stringify(state))
     } else {
       window.localStorage.removeItem('token')
     }
   }, [state])
 
+  const httpLink = createHttpLink({
+    uri: process.env.REACT_APP_HASURA_URL
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    if (state) {
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${state}`
+        }
+      };
+    } else {
+      return {
+        headers: {
+          ...headers
+        }
+      };
+    }
+  });
+
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query);
+      return (
+        kind === 'OperationDefinition' &&
+        operation === 'subscription'
+      );
+    },
+    authLink.concat(httpLink)
+  );
+
+  const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+  });
+
   return (
-    <Context.Provider value={{state, setState}}>
-      {children}
+    <Context.Provider value={{ state, setState }}>
+      <ApolloProvider client={client}>{children}</ApolloProvider>
     </Context.Provider>
   )
 }
